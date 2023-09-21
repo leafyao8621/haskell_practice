6c777041ec0a5d3f13d7cf7e175d4e8053089fa5
Yi Yao <leaf_yao8621@hotmail.com>
2023-09-21 10:04:40 -0700

    snake to camel

--- prime_cnt/prime_cnt.hs
+++ prime_cnt/prime_cnt.hs
@@ -1,17 +1,17 @@
-is_prime_helper :: Int -> Int -> Int
-is_prime_helper n i | n /= i = -1
-is_prime_helper n i | n < 2 = 0
-is_prime_helper n i =
+isPrimeHelper :: Int -> Int -> Int
+isPrimeHelper n i | n /= i = -1
+isPrimeHelper n i | n < 2 = 0
+isPrimeHelper n i =
     f i where
     f 2 = 1
     f x = if n `mod` (x - 1) /= 0 then f (x - 1) else 0
 
-is_prime :: Int -> Int
-is_prime x = is_prime_helper x x
+isPrime :: Int -> Int
+isPrime x = isPrimeHelper x x
 
-prime_cnt :: Int -> Int
-prime_cnt x | x < 2 = 0
-prime_cnt 2 = 1
-prime_cnt x = is_prime x + prime_cnt (x - 1)
+primeCnt :: Int -> Int
+primeCnt x | x < 2 = 0
+primeCnt 2 = 1
+primeCnt x = isPrime x + primeCnt (x - 1)
 
-main = print(prime_cnt 100)
+main = print(primeCnt 100)
