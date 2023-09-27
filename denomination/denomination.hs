7fc2d72f25b8deab4ed7a51ee6ac3c7f51dc21fd
Yi Yao <leaf_yao8621@hotmail.com>
2023-09-27 11:35:09 -0700

    Updates

--- denomination/denomination.hs
+++ denomination/denomination.hs
@@ -3,11 +3,11 @@ getDenomination x d | x < 0 = error "Negative value"
 getDenomination x [] = []
 getDenomination x d =
     f [d] where
-    f [y:ys] = [x `div` y] ++ (getDenomination (x `mod` y) ys)
+    f [y:ys] = x `div` y : getDenomination (x `mod` y) ys
 
 hasExactChange :: Int -> [Int] -> Bool
 hasExactChange x d | x < 0 = error "Negative value"
-hasExactChange x [] = (x == 0)
+hasExactChange x [] = x == 0
 hasExactChange x d =
     f [d] where
     f [y:ys] = hasExactChange (x `mod` y) ys
